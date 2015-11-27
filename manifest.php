e2c88b242c38df009eba8a3dc2fe87a14abe954f
Joel Bout <joel@taotesting.com>
2015-11-27 16:51:34 +0100

    semantic versioning

--- manifest.php
+++ manifest.php
@@ -24,7 +24,7 @@ return array(
 	'label' => 'Delivery Management',
 	'description' => 'Manages deliveries using the ontology',
     'license' => 'GPL-2.0',
-    'version' => '0.9',
+    'version' => '0.1',
 	'author' => 'Open Assessment Technologies SA',
 	'requires' => array(
         'taoGroups' => '>=2.7.1',
