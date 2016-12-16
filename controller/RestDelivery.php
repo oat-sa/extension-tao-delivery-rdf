a640c475752ee85c55443ef5a9aae68e57aa17c4
Aleh Hutnikau <hutnikau@1pt.com>
2016-12-16 10:26:30 +0300

    Create action for generation delivery through the task queue

--- controller/RestDelivery.php
+++ controller/RestDelivery.php
@@ -21,6 +21,7 @@ namespace oat\taoDeliveryRdf\controller;
 use oat\taoDeliveryRdf\model\tasks\CompileDelivery;
 use oat\tao\model\TaskQueueActionTrait;
 use oat\oatbox\task\Task;
+use oat\taoDeliveryRdf\model\SimpleDeliveryFactory;
 
 class RestDelivery extends \tao_actions_RestController
 {
@@ -33,10 +34,44 @@ class RestDelivery extends \tao_actions_RestController
     const TASK_ID_PARAM = 'id';
 
     /**
-     * Put task to generate a delivery from test uri to the task queue
+     * Generate a delivery from test uri
      * Test uri has to be set and existing
      */
     public function generate()
+    {
+        try {
+            if (!$this->hasRequestParameter(self::REST_DELIVERY_TEST_ID)) {
+                throw new \common_exception_MissingParameter(self::REST_DELIVERY_TEST_ID, $this->getRequestURI());
+            }
+
+            $test = new \core_kernel_classes_Resource($this->getRequestParameter(self::REST_DELIVERY_TEST_ID));
+            if (!$test->exists()) {
+                throw new \common_exception_NotFound('Unable to find a test associated to the given uri.');
+            }
+
+            $label = 'Delivery of ' . $test->getLabel();
+            $deliveryClass = new \core_kernel_classes_Class(CLASS_COMPILEDDELIVERY);
+
+            /** @var \common_report_Report $report */
+            $report = SimpleDeliveryFactory::create($deliveryClass, $test, $label);
+
+            if ($report->getType() == \common_report_Report::TYPE_ERROR) {
+                \common_Logger::i('Unable to generate delivery execution ' .
+                    'into taoDeliveryRdf::RestDelivery for test uri ' . $test->getUri());
+                throw new \common_Exception('Unable to generate delivery execution.');
+            }
+            $delivery = $report->getData();
+            $this->returnSuccess(array('delivery' => $delivery->getUri()));
+        } catch (\Exception $e) {
+            $this->returnFailure($e);
+        }
+    }
+
+    /**
+     * Put task to generate a delivery from test uri to the task queue
+     * Test uri has to be set and existing
+     */
+    public function generateDeferred()
     {   
         try {
             if (!$this->hasRequestParameter(self::REST_DELIVERY_TEST_ID)) {
