c0b1a4bd364fc5b763660331610b64f72ac873cc
siwane <camille@taotesting.com>
2017-03-27 12:49:59 +0000

    Bump to version 2.0.0

--- manifest.php
+++ manifest.php
@@ -24,7 +24,7 @@ return array(
 	'label'       => 'Delivery Management',
 	'description' => 'Manages deliveries using the ontology',
     'license'     => 'GPL-2.0',
-    'version'     => '1.17.0',
+    'version'     => '2.0.0',
 	'author'      => 'Open Assessment Technologies SA',
 	'requires'    => array(
         'tao'         => '>=7.69.2',
