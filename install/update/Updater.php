2277403d7b62e06c80359aa7868dbb55b3545975
Joel Bout <joel@taotesting.com>
2017-07-19 11:49:59 +0200

    Version bump

--- install/update/Updater.php
+++ install/update/Updater.php
@@ -143,6 +143,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('3.4.0');
         }
         
-        $this->skip('3.4.0', '3.6.0');
+        $this->skip('3.4.0', '3.7.0');
     }
 }
--- manifest.php
+++ manifest.php
@@ -24,7 +24,7 @@ return array(
 	'label'       => 'Delivery Management',
 	'description' => 'Manages deliveries using the ontology',
     'license'     => 'GPL-2.0',
-    'version'     => '3.6.0',
+    'version'     => '3.7.0',
 	'author'      => 'Open Assessment Technologies SA',
 	'requires'    => array(
         'generis'     => '>=3.36.0',
