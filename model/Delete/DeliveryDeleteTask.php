d937f29674763994cc7997b90626ac78af03f502
Ionut Padurariu <ionut@taotesting.com>
2018-01-24 14:40:10 +0100

    couple action to UI add task in the background queue

---
+++ model/Delete/DeliveryDeleteTask.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2018 (original work) Open Assessment Technologies SA;
+ *
+ */
+
+namespace oat\taoDeliveryRdf\model\Delete;
+
+use oat\oatbox\extension\AbstractAction;
+
+class DeliveryDeleteTask extends AbstractAction implements \JsonSerializable
+{
+    /**
+     * @param $params
+     * @return \common_report_Report
+     * @throws \Exception
+     */
+    public function __invoke($params)
+    {
+        if (!isset($params['deliveryId'])) {
+            throw new \common_exception_MissingParameter('Missing parameter `deliveryId` in ' . static::class);
+        }
+
+        /** @var DeliveryDeleteService $deleteDeliveryService */
+        $deleteDeliveryService = $this->getServiceLocator()->get(DeliveryDeleteService::SERVICE_ID);
+        $deleteDeliveryService->execute(new DeliveryDeleteRequest($params['deliveryId']));
+
+        return $deleteDeliveryService->getReport();
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function jsonSerialize()
+    {
+        return __CLASS__;
+    }
+}
