84dee4edb7acad42042350f323c3b5232a60d116
Ionut Padurariu <ionut@taotesting.com>
2018-02-06 18:12:49 +0100

    bump version

--- manifest.php
+++ manifest.php
@@ -29,7 +29,7 @@ return array(
   'label'       => 'Delivery Management',
   'description' => 'Manages deliveries using the ontology',
   'license'     => 'GPL-2.0',
-  'version'     => '4.2.0',
+  'version'     => '4.3.0',
 	'author'      => 'Open Assessment Technologies SA',
 	'requires'    => array(
 	    'generis'     => '>=5.11.0',
