4ca1fff8cbd56c978816f59ed329e99bf3b8bb65
Sam Sipasseuth <sam@taotesting.com>
2018-06-14 17:25:03 +0200

    updated dependency to tao core

--- manifest.php
+++ manifest.php
@@ -33,7 +33,7 @@ return array(
 	'author'      => 'Open Assessment Technologies SA',
 	'requires'    => array(
 	    'generis'     => '>=6.14.0',
-        'tao'         => '>=19.8.0',
+        'tao'         => '>=19.9.0',
         'taoGroups'   => '>=2.7.1',
         'taoTests'    => '>=7.9.0',
         'taoQtiTest'  => '>=18.6.0',
