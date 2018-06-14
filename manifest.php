bb04ad7d38195df3969e56466bb1a02d254ae159
Aleh Hutnikau <hutnikau@1pt.com>
2018-06-14 18:24:08 +0300

    add 4.6.0.1 version

--- manifest.php
+++ manifest.php
@@ -29,7 +29,7 @@ return array(
   'label'       => 'Delivery Management',
   'description' => 'Manages deliveries using the ontology',
   'license'     => 'GPL-2.0',
-  'version'     => '5.2.2',
+  'version'     => '5.2.3',
 	'author'      => 'Open Assessment Technologies SA',
 	'requires'    => array(
 	    'generis'     => '>=6.14.0',
