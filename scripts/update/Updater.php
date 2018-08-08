0ac804a0025bac16183fa3e037f7b6319091e2d5
alexey <ale441v@gmail.com>
2018-08-08 18:21:23 +0300

    Changed version after fixing tests

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -235,6 +235,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('5.2.2');
         }
 
-        $this->skip('5.2.2', '5.5.0');
+        $this->skip('5.2.2', '5.5.1');
     }
 }
