99c335e8a7d1f884e6df49f162cb1a1586e3a0df
Aleksej Tikhanovich <aleksej@taotesting.com>
2018-11-19 13:32:43 +0300

    Bump version

--- manifest.php
+++ manifest.php
@@ -29,10 +29,10 @@ return array(
   'label'       => 'Delivery Management',
   'description' => 'Manages deliveries using the ontology',
   'license'     => 'GPL-2.0',
-  'version'     => '7.3.0',
+  'version'     => '8.0.0',
 	'author'      => 'Open Assessment Technologies SA',
 	'requires'    => array(
-	    'generis'     => '>=6.14.0',
+	    'generis'     => '>=8.0.0',
         'tao'         => '>=21.0.0',
         'taoGroups'   => '>=4.0.0',
         'taoTests'    => '>=8.0.0',
