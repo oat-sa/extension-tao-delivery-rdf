95e2ccb7400120d28cb6b4a8464b7e746b2f8717
Martin Nicholson <martin@taotesting.com>
2019-07-30 13:33:27 +0200

    Added grunt testrunner task

---
+++ views/build/grunt/test.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    var watch       = grunt.config('watch') || {};
+    var qunit       = grunt.config('qunit') || {};
+    var testUrl     = 'http://127.0.0.1:' + grunt.option('testPort');
+    var root        = grunt.option('root');
+
+    var testRunners = root + '/taoDeliveryRdf/views/js/test/**/test.html';
+    var testFiles = root + '/taoDeliveryRdf/views/js/test/**/test.js';
+
+    //extract unit tests
+    var extractTests = function extractTests(){
+        return grunt.file.expand([testRunners]).map(function(path){
+            return path.replace(root, testUrl);
+        });
+    };
+
+    /**
+     * tests to run
+     */
+    qunit.taodeliveryrdf = {
+        options : {
+            console : true,
+            urls : extractTests()
+        }
+    };
+
+
+    watch.taodeliveryrdf = {
+        files : [testRunners, testFiles],
+        tasks : ['qunit:taodeliveryrdf'],
+        options : {
+            debounceDelay : 10000
+        }
+    };
+
+    grunt.config('qunit', qunit);
+    grunt.config('watch', watch);
+
+    // bundle task
+    grunt.registerTask('taodeliveryrdftest', ['qunit:taodeliveryrdf']);
+};
