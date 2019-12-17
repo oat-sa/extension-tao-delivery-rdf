5add4c911d7610738081cdb58b7313f7a21ae65b
Sasha <oleksandr@taotesting.com>
2019-12-17 18:44:15 +0100

    Implement AssemblyFilesReader with conversion of compiled test file from PHP to XML.

---
+++ model/assembly/AssemblyFilesReader.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2019  (original work) Open Assessment Technologies SA;
+ */
+
+namespace oat\taoDeliveryRdf\model\assembly;
+
+use oat\oatbox\filesystem\Directory;
+use oat\oatbox\filesystem\File;
+use oat\oatbox\service\ConfigurableService;
+use tao_models_classes_service_StorageDirectory;
+
+class AssemblyFilesReader extends ConfigurableService implements AssemblyFilesReaderInterface
+{
+    /**
+     * @param tao_models_classes_service_StorageDirectory $directory
+     * @return \Generator|array In format $filePath => StreamInterface
+     */
+    public function getFiles(tao_models_classes_service_StorageDirectory $directory)
+    {
+        $compiledTestFilename = 'compact-test.php';
+        $iterator = $directory->getFlyIterator(Directory::ITERATOR_FILE | Directory::ITERATOR_RECURSIVE);
+        /* @var $file File */
+        foreach ($iterator as $file) {
+            if (strpos($file->getBasename(), $compiledTestFilename) !== false) {
+                $file = $this->getServiceLocator()->get(CompiledTestConverterService::class)->convertPhpToXml($file, $directory);
+                $fileStream = $file->readPsrStream();
+                $file->delete();
+            } else {
+                $fileStream = $file->readPsrStream();
+            }
+
+            yield $file->getPrefix() => $fileStream;
+        }
+    }
+}
---
+++ model/assembly/AssemblyFilesReaderInterface.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2019  (original work) Open Assessment Technologies SA;
+ */
+
+namespace oat\taoDeliveryRdf\model\assembly;
+
+use tao_models_classes_service_StorageDirectory;
+
+interface AssemblyFilesReaderInterface
+{
+    const SERVICE_ID = 'taoDeliveryRdf';
+
+    /**
+     * @param tao_models_classes_service_StorageDirectory $directory
+     *
+     * @return array Array in format $filePath => StreamInterface
+     */
+    public function getFiles(tao_models_classes_service_StorageDirectory $directory);
+}
