a3fc3f49ba5fd067eff06d9b45848d8d6f947ee1
Bout <joel@taotesting.com>
2020-03-17 16:17:57 +0100

    Bump v11.2.0

--- manifest.php
+++ manifest.php
@@ -30,7 +30,7 @@ return [
   'label'       => 'Delivery Management',
   'description' => 'Manages deliveries using the ontology',
   'license'     => 'GPL-2.0',
-  'version'     => '11.1.0',
+  'version'     => '11.2.0',
     'author'      => 'Open Assessment Technologies SA',
     'requires'    => [
         'generis'     => '>=12.5.0',
