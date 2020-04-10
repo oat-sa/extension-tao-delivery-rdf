4f8ddea2ba726bbdc06ce175713dfa6313717541
Aleh Hutnikau <aleh.hutnikau@1pt.com>
2020-04-10 16:43:55 +0300

    remove Ontology impmenetnation of result service

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -263,6 +263,6 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('10.0.0');
         }
 
-        $this->skip('10.0.0', '11.4.1');
+        $this->skip('10.0.0', '11.5.0');
     }
 }
