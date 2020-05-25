8b6a8bc875bff61e3752593ce64c79784031d2f5
Aleh Hutnikau <aleh.hutnikau@1pt.com>
2020-05-25 10:01:59 +0300

    fix phpdoc of DeliveryDeleteAction

--- model/Delete/DeliveryDeleteAction.php
+++ model/Delete/DeliveryDeleteAction.php
@@ -15,14 +15,15 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2018 (original work) Open Assessment Technologies SA;
+ * Copyright (c) 2020 (original work) Open Assessment Technologies SA;
  *
  */
+
+declare(strict_types = 1);
 
 namespace oat\taoDeliveryRdf\model\Delete;
 
 use oat\oatbox\extension\AbstractAction;
-use oat\tao\model\taskQueue\QueueDispatcher;
 
 /**
  * Usage example:
