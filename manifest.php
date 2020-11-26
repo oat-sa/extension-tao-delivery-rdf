835aaeb47d5fd0f9d349762a3c26531e8383d533
Andrey Niahrou <Andrei.Niahrou@1pt.com>
2020-11-26 12:50:55 +0300

    bump version

--- manifest.php
+++ manifest.php
@@ -36,7 +36,7 @@ return [
   'label'       => 'Delivery Management',
   'description' => 'Manages deliveries using the ontology',
   'license'     => 'GPL-2.0',
-  'version'     => '13.1.0',
+  'version'     => '13.1.0.1',
     'author'      => 'Open Assessment Technologies SA',
     'requires'    => [
         'generis'     => '>=12.32.1',
