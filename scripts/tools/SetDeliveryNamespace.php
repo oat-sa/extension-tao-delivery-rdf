1f6f985b572ffac08df9107bd5b4324fe05f33a7
Sergei Mikhailov <sergei.mikhailov@taotesting.com>
2021-06-18 16:40:07 +0200

    feat: implement `SetDeliveryNamespace` script, allowing to redefine remotely published Delivery resources' namespace
    --
    TR-1357
    
    (cherry picked from commit ba6022f603fcb9945696c79f9a800e9ca763f1ac)

---
+++ scripts/tools/SetDeliveryNamespace.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2021 (original work) Open Assessment Technologies SA;
+ *
+ * @author Sergei Mikhailov <sergei.mikhailov@taotesting.com>
+ */
+
+declare(strict_types=1);
+
+namespace oat\taoDeliveryRdf\scripts\tools;
+
+use Exception;
+use InvalidArgumentException;
+use oat\oatbox\extension\script\ScriptAction;
+use oat\oatbox\reporting\Report;
+use oat\taoDeliveryRdf\model\DeliveryFactory;
+
+/**
+ * Usage:
+ *
+ * sudo -u www-data php index.php 'oat\taoDeliveryRdf\scripts\tools\SetDeliveryNamespace' -n https://taotesting.com
+ */
+class SetDeliveryNamespace extends ScriptAction
+{
+    protected function provideUsage(): array
+    {
+        return [
+            'prefix'      => 'h',
+            'longPrefix'  => 'help',
+            'description' => 'Prints this message',
+        ];
+    }
+
+    protected function provideOptions(): array
+    {
+        return [
+            'namespace' => [
+                'prefix'      => 'n',
+                'longPrefix'  => 'namespace',
+                'required'    => true,
+                'description' => 'A custom namespace for remotely published Delivery resources',
+            ],
+        ];
+    }
+
+    protected function run(): Report
+    {
+        $namespace = $this->getNamespace();
+
+        $deliveryFactory = $this->getDeliveryFactory();
+        $deliveryFactory->setOption(DeliveryFactory::OPTION_NAMESPACE, $namespace);
+
+        try {
+            $this->getServiceManager()->register($deliveryFactory::SERVICE_ID, $deliveryFactory);
+        } catch (Exception $exception) {
+            return Report::createError(
+                "Failed to set \"$namespace\" Delivery namespace.",
+                null,
+                [Report::createInfo($exception->getMessage())]
+            );
+        }
+
+        return Report::createSuccess("Registered \"$namespace\" Delivery namespace.");
+    }
+
+    protected function provideDescription(): string
+    {
+        return 'TAO DeliveryRDF - Set up remotely published Delivery resource namespace';
+    }
+
+    private function getNamespace(): string
+    {
+        $namespace = rtrim($this->getOption('namespace'), '#');
+
+        if ($namespace === LOCAL_NAMESPACE) {
+            throw new InvalidArgumentException(
+                "Overridden namespace value must be different from a local one, \"$namespace\" given"
+            );
+        }
+
+        return $namespace;
+    }
+
+    private function getDeliveryFactory(): DeliveryFactory
+    {
+        return $this->getServiceLocator()->get(DeliveryFactory::class);
+    }
+}
