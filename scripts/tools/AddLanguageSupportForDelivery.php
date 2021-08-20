d61b7be0771b274a50587e8a52c315a24161bf53
Ricardo Quintanilha <ricardo.quintanilha@taotesting.com>
2021-08-20 10:28:11 +0200

    chore: Rename DeliveryAssemblyService to AddLanguageSupportForDelivery and move it to `scripts/tools` folder

---
+++ scripts/tools/AddLanguageSupportForDelivery.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2021 (original work) Open Assessment Technologies SA;
+ *
+ *
+ */
+
+declare(strict_types=1);
+
+namespace oat\taoDeliveryRdf\scripts\tools;
+
+use Exception;
+use oat\generis\model\data\import\RdfImporter;
+use oat\oatbox\extension\script\ScriptAction;
+use oat\oatbox\reporting\Report;
+use oat\oatbox\service\ServiceManager;
+
+/**
+ * Usages:
+ *
+ * php index.php "oat\taoDeliveryRdf\scripts\tools\AddLanguageSupportForDelivery"
+ */
+class AddLanguageSupportForDelivery extends ScriptAction
+{
+    private const APP_ROOT_PATH = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR;
+    private const RDF_FILE_PATH = self::APP_ROOT_PATH . 'install' . DIRECTORY_SEPARATOR . 'ontology' . DIRECTORY_SEPARATOR . 'taodelivery_language.rdf';
+
+    protected function provideOptions(): array
+    {
+        return [];
+    }
+
+    protected function provideDescription(): array
+    {
+        return [];
+    }
+
+    protected function run(): Report
+    {
+        $serviceManager = ServiceManager::getServiceManager();
+
+        try {
+            $importer = $serviceManager->get(RdfImporter::class);
+            $importer->importFile(self::RDF_FILE_PATH);
+        } catch (Exception $exception) {
+            return Report::createError(
+                'Failed to run the script.',
+                null,
+                [Report::createInfo($exception->getMessage())]
+            );
+        }
+
+        return Report::createSuccess("Language support for deliveries has been added successfully.");
+    }
+}
