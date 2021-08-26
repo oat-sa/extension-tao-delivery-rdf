bc0f7ef78ea848a4f5b282f4943718f5b432dee0
Sergei Mikhailov <sergei.mikhailov@taotesting.com>
2021-08-26 10:51:46 +0200

    fix: `CleanUp` script DocBlock
    ---
    [TR-1661](https://oat-sa.atlassian.net/browse/TR-1661)
    
    (cherry picked from commit c3a50a18ebe639aca21a37a23098a308ff1d54f6)

--- scripts/tools/CleanUp.php
+++ scripts/tools/CleanUp.php
@@ -15,7 +15,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2017-2021 (original work) Open Assessment Technologies SA;
+ * Copyright (c) 2021 (original work) Open Assessment Technologies SA;
+ *
+ * @author Sergei Mikhailov <sergei.mikhailov@taotesting.com>
  */
 
 declare(strict_types=1);
