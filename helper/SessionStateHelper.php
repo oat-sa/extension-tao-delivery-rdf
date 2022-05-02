ff0cf479f7efb616c63e783acce7bc09005a64b3
poyuk <kyril.poyu@taotesting.com>
2022-05-02 17:57:47 +0200

    feat: test session restoration at execution reactivation event

--- helper/SessionStateHelper.php
+++ helper/SessionStateHelper.php
@@ -15,34 +15,74 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2018 (original work) Open Assessment Technologies SA;
+ * Copyright (c) 2018-2022 (original work) Open Assessment Technologies SA;
  *
  *
  */
 
 namespace oat\taoDeliveryRdf\helper;
 
+use common_Exception;
+use common_exception_Error;
+use common_exception_NotFound;
+use common_ext_ExtensionException;
+use oat\oatbox\log\LoggerService;
+use oat\oatbox\service\exception\InvalidServiceManagerException;
 use oat\oatbox\service\ServiceManager;
 use oat\taoDelivery\models\classes\execution\event\DeliveryExecutionReactivated;
+use oat\taoQtiTest\models\QtiTestExtractionFailedException;
 use oat\taoQtiTest\models\TestSessionService;
+use oat\taoQtiTest\models\TestSessionState\Api\TestSessionStateRestorationServiceInterface;
+use oat\taoQtiTest\models\TestSessionState\Exception\RestorationImpossibleException;
+use qtism\runtime\storage\common\StorageException;
 use qtism\runtime\tests\AssessmentTestSessionState;
 
 class SessionStateHelper
 {
     /**
      * @param DeliveryExecutionReactivated $event
-     * @throws \common_exception_Error
-     * @throws \common_exception_MissingParameter
-     * @throws \qtism\runtime\storage\common\StorageException
+     * @throws StorageException
+     * @throws common_Exception
+     * @throws common_exception_NotFound
+     * @throws common_ext_ExtensionException
+     * @throws InvalidServiceManagerException
+     * @throws QtiTestExtractionFailedException
+     * @throws common_exception_Error
      */
     public static function onExecutionReactivation(DeliveryExecutionReactivated $event)
     {
-        /** @var TestSessionService $testSessionService */
-        $testSessionService = ServiceManager::getServiceManager()->get(TestSessionService::SERVICE_ID);
-        $session = $testSessionService->getTestSession($event->getDeliveryExecution());
+        $testSessionService = self::getTestSessionService();
+        $deliveryExecution = $event->getDeliveryExecution();
+        $session = $testSessionService->getTestSession($deliveryExecution);
+        if (!$session) {
+            try {
+                self::getTestSessionStateRestorationService()->restore($event->getDeliveryExecution());
+            } catch (RestorationImpossibleException $e) {
+                self::getLoggerService()->warning($e->getMessage());
+                return;
+            }
+
+            $session = $testSessionService->getTestSession($deliveryExecution);
+        }
+
         if ($session) {
             $session->setState(AssessmentTestSessionState::SUSPENDED);
             $testSessionService->persist($session);
         }
+    }
+
+    private static function getTestSessionService(): TestSessionService
+    {
+        return ServiceManager::getServiceManager()->getContainer()->get(TestSessionService::SERVICE_ID);
+    }
+
+    private static function getTestSessionStateRestorationService(): TestSessionStateRestorationServiceInterface
+    {
+        return ServiceManager::getServiceManager()->getContainer()->get(TestSessionStateRestorationServiceInterface::class);
+    }
+
+    private static function getLoggerService(): LoggerService
+    {
+        return ServiceManager::getServiceManager()->getContainer()->get(LoggerService::SERVICE_ID);
     }
 }
